3095c4bffa09cea14e2c756d47c5bd1ee34ce087
Roy <rstraub@xebia.com>
2023-09-07 13:10:34 +0200

    Scalacheck/test

--- src/test/scala/nl/codecraftr/scala/kata/CanarySpec.scala
+++ src/test/scala/nl/codecraftr/scala/kata/CanarySpec.scala
@@ -4,20 +4,30 @@ import org.scalatest.BeforeAndAfter
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
 class CanarySpec
     extends AnyWordSpec
     with Matchers
     with MockitoSugar
+    with ScalaCheckPropertyChecks
     with BeforeAndAfter {
 
   before {
     println("fires before every test")
   }
 
-  "canary" should {
+  "scalatest" should {
     "run tests" in {
       true shouldBe true
+    }
+  }
+
+  "scalacheck" should {
+    "run property-based tests" in {
+      forAll { (a: Int, b: Int) =>
+        a + b shouldEqual b + a
+      }
     }
   }
 }
