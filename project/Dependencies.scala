85c17a537da036452a2b5314022caea2915d032a
Roy Straub <roy@codecraftr.nl>
2023-09-10 00:02:54 +0000

    Update mockito-4-11, scalacheck-1-17 to 3.2.17.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16" % Test
   lazy val scalaCheck =
-    "org.scalatestplus" %% "scalacheck-1-17" % "3.2.16.0" % Test
+    "org.scalatestplus" %% "scalacheck-1-17" % "3.2.17.0" % Test
 
   lazy val catsCore = "org.typelevel" %% "cats-core" % "2.9.0"
-  lazy val mockito = "org.scalatestplus" %% "mockito-4-11" % "3.2.16.0" % Test
+  lazy val mockito = "org.scalatestplus" %% "mockito-4-11" % "3.2.17.0" % Test
 }
