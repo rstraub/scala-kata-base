88b1edb4e314721303ca5175b60d2f0705e8ab1e
Roy Straub <roy@codecraftr.nl>
2023-09-10 00:03:09 +0000

    Update cats-core to 2.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,6 +5,6 @@ object Dependencies {
   lazy val scalaCheck =
     "org.scalatestplus" %% "scalacheck-1-17" % "3.2.16.0" % Test
 
-  lazy val catsCore = "org.typelevel" %% "cats-core" % "2.9.0"
+  lazy val catsCore = "org.typelevel" %% "cats-core" % "2.10.0"
   lazy val mockito = "org.scalatestplus" %% "mockito-4-11" % "3.2.16.0" % Test
 }
